6d0b8766279675eb1063fed0b81abb05279bc7e9
ilovekego <ilovekenya001@gmail.com>
2026-08-29 12:51:18 +0300

    Update status.js

--- api/status.js
+++ api/status.js
@@ -4,7 +4,7 @@ module.exports = async (req, res) => {
   await connectDB();
   try {
     const count = await Contact.countDocuments();
-    res.json({ count, max: 800, isFull: count >= 800 });
+    res.json({ count, max: 500, isFull: count >= 500 });
   } catch (err) {
     res.status(500).json({ error: "Server error" });
   }
